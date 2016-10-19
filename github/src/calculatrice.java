c269535d2e8a00141817583f3d26041ed4ed32b7
aelouassif <elouassif.28@gmail.com>
2016-10-19 17:56:05 +0100

    ajout des boutons

--- github/src/calculatrice.java
+++ github/src/calculatrice.java
@@ -12,16 +12,81 @@ class maFenetre extends JFrame
 		this.setBounds(200,200,335,380);
 		this.setResizable(false);
 		
-		
+		//pour l'affichege
 		Container C = getContentPane();
 		C.setLayout(new FlowLayout());
 		monEcrant = new ecrant();
 		monEcrant.setVisible(true);
 		C.add(monEcrant);
 		
+		//pour les button
+		for(int i=1;i<=24;i++)
+		{
+			if(i<=18 && 1<=(i%6) && (i%6)<=3 )
+				monButton = new button(""+(i%6+3*(i/6)));
+			else
+			{
+				switch(i)
+				{
+					case 4:
+						monButton = new button("÷");
+						break;
+					case 10:
+						monButton = new button("*");
+						break;
+					case 11:
+						monButton = new button("(");
+						break;
+					case 12:
+						monButton = new button(")");
+						break;
+					case 16:
+						monButton = new button("-");
+						break;
+					case 17:
+						monButton = new button("x²");
+						break;
+					case 18:
+						monButton = new button("√");
+						break;
+					case 19:
+						monButton = new button("0");
+						break;
+					case 20:
+						monButton = new button(".");
+						break;
+					case 21:
+						monButton = new button("%");
+						break;
+					case 22:
+						monButton = new button("+");
+						break;
+					default:
+						monButton = new button("");
+						break;
+				
+				}
+			}
+			C.add(monButton);
+ 		}
 	}
 	private ecrant monEcrant;
+	private button monButton;
 }
+class button extends JButton implements ActionListener
+{
+	button(String mot)
+	{
+		super(mot);
+		this.setPreferredSize(new Dimension(50,40));
+		this.addActionListener(this);
+	}
+	public void actionPerformed(ActionEvent ev)
+	{
+		System.out.println(ev.getActionCommand());
+	}
+}
+
 class ecrant extends JTextField
 {
 	ecrant()
